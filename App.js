9c266598c03bf5b2aac0c3021f3bd994aade313d
Isidro Moreno <imoreno.itservices@gmail.com>
2022-04-14 20:09:38 -0300

    Initial commit.

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import type { Node } from 'react';
+import React from 'react';
+import {
+  SafeAreaView, StatusBar, Text,
+  useColorScheme,
+  View
+} from 'react-native';
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+import HomeScreen from './src/screens/HomeScreen';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/screens/HomeScreen/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomeScreen = (props) => {
+  return (<View>
+    <Text>notJust components</Text>
+  </View>);
+};
+
+export default HomeScreen;
